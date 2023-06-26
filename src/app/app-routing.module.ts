3f0b42c1fe25bd19bc6335774e1ee93ee0ced543
jatinder <jatindersingh93@gmail.com>
2023-06-26 18:04:07 -0500

    clean code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { AddProductComponent } from './components/add-product/add-product.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ProductListComponent },
-  { path: 'proudcts/:id', component: ProductDetailsComponent },
-  { path: 'add', component: AddProductComponent }  
 ];
 
 @NgModule({
